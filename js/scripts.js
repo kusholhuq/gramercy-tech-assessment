a13f9df5f0e0736d03eb2a4075fbd8fc5e9162e0
kusholhuq <kusholhuq@gmail.com>
2022-02-05 12:05:15 -0800

    buttons respond to active

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,8 @@
 
 $(document).ready(function() {
 
-    // function injectBanner(){
-    //     const h2 = document.querySelector("h2");
-    //     const forms = document.querySelectorAll("form");
-    //     const formSignUp = forms[0];
-    //     // const formSignIn = forms[1];
-
-    //     const banner = document.createElement("img");
-    //     banner.setAttribute("src", "../assets_from_client/assets/WSJ_Horizontal.png")
-    //     banner.classList.add("banner");
-    //     formSignUp.insertBefore(banner, formSignUp.firstChild);
-
-    //     // formSignIn.insertBefore(banner, formSignUp.firstChild);
-    //     // formSignUp.classList.add("grey-background");
-    // }
-
-    //handleclick function to inject img on sign in
-
-    //handleclick to highlight active tab
-
     function app(){
-        // setFonts();
-        // pageAdjustments();
-        injectBanner();
+
     }
 
     app();
