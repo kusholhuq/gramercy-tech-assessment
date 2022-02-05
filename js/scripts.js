3b30c3ff1acb5c1a09234c3165907bb48d3a453c
kusholhuq <kusholhuq@gmail.com>
2022-02-05 11:44:01 -0800

    replaced dynamic js stylings with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,29 @@
 // Write your JS below
 
 $(document).ready(function() {
-    console.log('Document ready')
 
-    function setFonts(){
-        const h1 = document.querySelector("h1");
-        h1.classList.add("sharp-sans-black");
+    // function injectBanner(){
+    //     const h2 = document.querySelector("h2");
+    //     const forms = document.querySelectorAll("form");
+    //     const formSignUp = forms[0];
+    //     // const formSignIn = forms[1];
 
-        const h2 = document.querySelector("h2");
-        h2.classList.add("sharp-sans-black");
-    }
+    //     const banner = document.createElement("img");
+    //     banner.setAttribute("src", "../assets_from_client/assets/WSJ_Horizontal.png")
+    //     banner.classList.add("banner");
+    //     formSignUp.insertBefore(banner, formSignUp.firstChild);
 
-    function pageAdjustments(){
-        const body = document.querySelector("body");
-        body.classList.add("center");
-    }
+    //     // formSignIn.insertBefore(banner, formSignUp.firstChild);
+    //     // formSignUp.classList.add("grey-background");
+    // }
 
-    function injectBanner(){
-        const h2 = document.querySelector("h2");
-        const forms = document.querySelectorAll("form");
-        const formSignUp = forms[0];
-        const formSignIn = forms[1];
+    //handleclick function to inject img on sign in
 
-        const banner = document.createElement("img");
-        banner.setAttribute("src", "../assets_from_client/assets/WSJ_Horizontal.png")
-        banner.classList.add("banner");
-        formSignUp.insertBefore(banner, formSignUp.firstChild);
-        formSignIn.insertBefore(banner, formSignUp.firstChild);
-
-        formSignUp.classList.add("grey-background");
-    }
+    //handleclick to highlight active tab
 
     function app(){
-        setFonts();
-        pageAdjustments();
+        // setFonts();
+        // pageAdjustments();
         injectBanner();
     }
 
